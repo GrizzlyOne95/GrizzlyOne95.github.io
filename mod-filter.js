8ea4d931bcfb7e7979d2329670f528e9e305e199
GrizzlyOne95 <143971298+GrizzlyOne95@users.noreply.github.com>
2026-09-03 09:39:48 -0500

    Load live sidebar status on Mods page

--- mod-filter.js
+++ mod-filter.js
@@ -69,4 +69,8 @@
 
   input.addEventListener('input', apply);
   apply();
+
+  const liveScript = document.createElement('script');
+  liveScript.src = './sidebar-live.js';
+  document.head.appendChild(liveScript);
 })();
